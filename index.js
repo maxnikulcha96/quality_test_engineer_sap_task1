305306e3a5763adee474745d62101223d6a4f5be
maxnikulcha96 <maxnikulcha96@gmail.com>
2021-06-16 13:30:04 +0200

    Variables converted to constants.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Service = require("@sap_oss/odata-library").Service;
 
-var northwindApiUrl = "https://services.odata.org/V2/OData/OData.svc/";
-var numberOfProducts = 10;
+const API_URL = "https://services.odata.org/V2/OData/OData.svc/";
+const NUM_OF_PRODUCTS = 10;
 
-var service = new Service(northwindApiUrl);
+var service = new Service(API_URL);
 
 service.init.then(() => {
-    return service.Products.get(numberOfProducts);
+    return service.Products.get(NUM_OF_PRODUCTS);
 }).then((products) => {
     for (index in products) {
         console.log(products[index].Name);
